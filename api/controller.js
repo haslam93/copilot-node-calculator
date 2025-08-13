db4c9b56aec06b27c857613f08083efdc4770863
Hammad Aslam <36418598+haslam93@users.noreply.github.com>
2025-08-13 13:47:18 -0400

    Update controller.js

--- api/controller.js
+++ api/controller.js
@@ -42,3 +42,10 @@ exports.calculate = function(req, res) {
 
   res.json({ result: operation(req.query.operand1, req.query.operand2) });
 };
+
+//Make a change
+  if (!req.query.operand2 ||
+      !req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+      req.query.operand2.replace(/[-0-9e]/g, '').length > 1) {
+    throw new Error("Invalid operand2: " + req.query.operand2);
+  }
